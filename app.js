68c53da65714d58105e93dcdc20bcf29181400cf
camilo@dill.cl-at-143347831018 <kmilo93sd@gmail.com>
2019-10-08 20:54:34 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express =  require('express');
+const parser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(parser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({
+        message : 'Welcome to lighthouse-microservice.',
+        status : {
+            general: true,
+        },
+    });
+});
+
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+
+const  launchChromeAndRunLighthouse = (url, opts, config = null) => {
+    return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
+      opts.port = chrome.port;
+      return lighthouse(url, opts, config).then(results => {
+        // use results.lhr for the JS-consumeable output
+        // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
+        // use results.report for the HTML/JSON/CSV output as a string
+        // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
+        return chrome.kill().then(() => results.lhr)
+      });
+    });
+  }
+
+app.get('/report', (request, response) => {
+    const {url} = request.body;
+    const opts =  {
+        chromeFlags: ['--show-paint-rects']
+      };
+    launchChromeAndRunLighthouse(url, opts)
+    .then(results => response.json({ data: results }))
+    .catch(error => response.json({error: error.message}));
+});
+
+
+module.exports = app;
